f0d9cb01ec1d529e580554c20de737e895470b57
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-19 19:36:36 -0700

    update

--- install.js
+++ install.js
@@ -1,14 +1,21 @@
 module.exports = async (kernel) => {
-  let cmd = "{{(platform === 'darwin' ? self.cmds.darwin : (['nvidia', 'amd'].includes(gpu) ? self.cmds[platform][gpu] : self.cmds[platform].cpu))}}"
-  if (kernel.gpu === 'nvidia' && kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
-    cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
-  }
+  let cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
   if (kernel.platform === 'darwin') {
     if (kernel.arch === "arm64") {
       cmd = "uv pip install torch torchaudio torchvision"
     } else {
       cmd = "uv pip install torch==2.1.2 torchaudio==2.1.2"
     }
+  } else {
+    if (kernel.gpu === 'nvidia') {
+      if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
+        cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+      } else {
+        cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121"
+      }
+    } else if (kernel.gpu === 'amd') {
+      cmd = "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm6.0"
+    } 
   }
   return {
     "cmds": {
